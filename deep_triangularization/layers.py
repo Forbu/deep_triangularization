1e38a10be89ab2a3ecb7f3346e186d9003aeca63
Adrien B <adri141193@yahoo.fr>
2023-06-27 09:29:35 +0000

    adding project structure

---
+++ deep_triangularization/layers.py
@@ -0,0 +1,45 @@
+"""
+In this module we will create the torch module that will optimize the linear layers of the network.
+"""
+import math
+
+import torch
+from torch import nn
+
+
+class Triangle(nn.Module):
+    """
+    Simple class that instead of doing a linear transformation, does a triangularization.
+    """
+
+    def __init__(self, in_dim, out_dim, upper=True):
+        super(Triangle, self).__init__()
+        self.in_dim = in_dim
+        self.out_dim = out_dim
+        self.weight = nn.Parameter(torch.Tensor(out_dim, in_dim))
+        self.bias = nn.Parameter(torch.Tensor(out_dim))
+
+        self.upper = upper
+
+        # compute a triangular mask
+        self.mask = (
+            torch.triu(torch.ones(out_dim, in_dim), diagonal=1)
+            if upper
+            else torch.tril(torch.ones(out_dim, in_dim), diagonal=-1)
+        )
+
+        # we register the mask as a buffer so that it is moved to the device along with the module
+        self.register_buffer("mask", self.mask)
+
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        nn.init.kaiming_uniform_(self.weight, a=math.sqrt(5))
+        fan_in, _ = nn.init._calculate_fan_in_and_fan_out(self.weight)
+        bound = 1 / math.sqrt(fan_in)
+        nn.init.uniform_(self.bias, -bound, bound)
+
+    def forward(self, input):
+        # we apply the triangular mask to the weight matrix
+        weight = self.weight * self.mask
+        return nn.functional.linear(input, weight, self.bias)
---
+++ deep_triangularization/models.py
@@ -0,0 +1,65 @@
+"""
+In this module we will create the torch module that will optimize the linear layers of the network.
+And also the module that will optimize the triangularization of the network.
+"""
+
+import math
+
+import torch
+from torch import nn
+
+from deep_triangularization.layers import Triangle
+
+class MLP_dense(nn.Module):
+    """
+    Simple MLP with dense layers.
+    """
+
+    def __init__(self, in_dim, out_dim, hidden_dim=128, num_layers=5):
+        super(MLP_dense, self).__init__()
+        self.in_dim = in_dim
+        self.out_dim = out_dim
+        self.hidden_dim = hidden_dim
+        self.num_layers = num_layers
+
+        self.layers = nn.ModuleList(
+            [
+                nn.Linear(in_dim, hidden_dim),
+                *[nn.Linear(hidden_dim, hidden_dim) for _ in range(num_layers - 2)],
+                nn.Linear(hidden_dim, out_dim),
+            ]
+        )
+
+    def forward(self, input):
+
+        for layer in self.layers[:-1]:
+            input = nn.functional.relu(layer(input))
+
+        return self.layers[-1](input)
+    
+class MLP_triangular(nn.Module):
+    """
+    MLP with triangular layers (hidden layers only).
+    """
+    
+        def __init__(self, in_dim, out_dim, hidden_dim=128, num_layers=5):
+            super(MLP_triangular, self).__init__()
+            self.in_dim = in_dim
+            self.out_dim = out_dim
+            self.hidden_dim = hidden_dim
+            self.num_layers = num_layers
+    
+            self.layers = nn.ModuleList(
+                [
+                    nn.Linear(in_dim, hidden_dim),
+                    *[Triangle(hidden_dim, hidden_dim) for _ in range(num_layers - 2)],
+                    nn.Linear(hidden_dim, out_dim),
+                ]
+            )
+    
+        def forward(self, input):
+    
+            for layer in self.layers[:-1]:
+                input = nn.functional.relu(layer(input))
+    
+            return self.layers[-1](input)
