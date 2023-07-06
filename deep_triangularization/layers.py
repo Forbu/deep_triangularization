0cf09342095100f89814d7d4cd12bcb92b621500
Adrien B <adri141193@yahoo.fr>
2023-07-06 22:45:10 +0000

    include a random permutation

--- deep_triangularization/layers.py
+++ deep_triangularization/layers.py
@@ -8,6 +8,7 @@ from torch import nn
 
 from einops import rearrange
 
+
 class Triangle(nn.Module):
     """
     Simple class that instead of doing a linear transformation, does a triangularization.
@@ -55,30 +56,52 @@ class Triangle(nn.Module):
         else:
             return nn.functional.linear(input, weight, self.bias)
 
+
 class HeadLinear(nn.Module):
     """
     HeadLinear class which performe diagonal block multiplication in a cleaver way
     """
-    def __init__(self, hidden_dim, nb_head):
+
+    def __init__(self, hidden_dim, nb_head, random_rows=True):
         """
         args:
             hidden_dim: int, the hidden dimension of the input
             nb_head: int, the number of head to use
-        
+
         """
         super(HeadLinear, self).__init__()
         self.hidden_dim = hidden_dim
         self.nb_head = nb_head
+        self.random_rows = random_rows
 
         assert hidden_dim % nb_head == 0, "hidden_dim must be divisible by nb_head"
 
-        self.weights = nn.Parameter(torch.randn(nb_head, hidden_dim//nb_head, hidden_dim//nb_head))
+        # if we want to randomize the rows of the mask
+        if random_rows:
+            # we register a random permutation of the rows as a buffer so that it is moved to the device along with the module
+            permutation = torch.randperm(hidden_dim)
+            inverse_permutation = torch.argsort(permutation)
+
+            self.register_buffer("permutation", permutation)
+            self.register_buffer("inverse_permutation", inverse_permutation)
+
+        self.weights = nn.Parameter(torch.randn(
+            nb_head, hidden_dim//nb_head, hidden_dim//nb_head))
 
     def forward(self, x):
         """
         forward pass of the HeadLinear module
         """
+        if self.random_rows:
+            # we apply the permutation to the input
+            x = x[:, self.permutation]
+
         x = rearrange(x, 'b (nb_head d) -> b nb_head d', nb_head=self.nb_head)
         out = torch.einsum('bmd,mdk->bmk', x, self.weights)
         out = rearrange(out, 'b nb_head k -> b (nb_head k)')
+
+        if self.random_rows:
+            # we apply the inverse permutation to the output
+            out = out[:, self.inverse_permutation]
+
         return out
