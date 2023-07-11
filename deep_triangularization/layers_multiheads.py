e4da5a7269613b8774be77e1d671f8b49b178fe4
Adrien B <forbu14@gmail.com>
2023-07-11 23:47:03 +0200

    Update layers_multiheads.py

--- deep_triangularization/layers_multiheads.py
+++ deep_triangularization/layers_multiheads.py
@@ -1,4 +1,3 @@
-
 import math
 
 import torch
@@ -35,16 +34,19 @@ class HeadLinear(nn.Module):
             self.register_buffer("permutation", permutation)
             self.register_buffer("inverse_permutation", inverse_permutation)
 
-        self.weights = nn.Parameter(torch.randn(
-            nb_head, hidden_dim//nb_head, hidden_dim//nb_head))
+        self.weights = nn.Parameter(
+            torch.randn(nb_head, hidden_dim // nb_head, hidden_dim // nb_head)
+        )
 
         self.bias = nn.Parameter(torch.zeros(hidden_dim))
-        
+
         # we initialize the weights and bias
         self.reset_parameters()
-    
+
     def reset_parameters(self):
-        
+        """
+        This method is used to initialize the weights and bias
+        """
         nn.init.kaiming_uniform_(self.weights, a=math.sqrt(5))
         fan_in, _ = nn.init._calculate_fan_in_and_fan_out(self.weights)
         bound = 1 / math.sqrt(fan_in)
@@ -58,50 +60,58 @@ class HeadLinear(nn.Module):
             # we apply the permutation to the input
             x = x[:, self.permutation]
 
-        x = rearrange(x, 'b (nb_head d) -> b nb_head d', nb_head=self.nb_head)
-        out = torch.einsum('bmd,mdk->bmk', x, self.weights)
-        out = rearrange(out, 'b nb_head k -> b (nb_head k)')
+        x = rearrange(x, "b (nb_head d) -> b nb_head d", nb_head=self.nb_head)
+        out = torch.einsum("bmd,mdk->bmk", x, self.weights)
+        out = rearrange(out, "b nb_head k -> b (nb_head k)")
 
-        #if self.random_rows:
-            # we apply the inverse permutation to the output
-         #   out = out[:, self.inverse_permutation]
+        # if self.random_rows:
+        # we apply the inverse permutation to the output
+        #   out = out[:, self.inverse_permutation]
 
         return out + self.bias.view(1, -1)
 
 
 class BlockDiagonalConv2d(nn.Module):
-
     def __init__(self, in_channels, out_channels, kernel_size, m):
         super(BlockDiagonalConv2d, self).__init__()
         self.in_channels = in_channels
         self.out_channels = out_channels
         self.kernel_size = kernel_size
         self.m = m
-        self.weights = nn.Parameter(torch.randn(
-            m, out_channels//m, in_channels//m, kernel_size, kernel_size))
+        self.weights = nn.Parameter(
+            torch.randn(
+                m, out_channels // m, in_channels // m, kernel_size, kernel_size
+            )
+        )
         self.bias = nn.Parameter(torch.zeros(out_channels))
 
         self.reset_parameters()
 
     def reset_parameters(self):
-
         nn.init.kaiming_uniform_(self.weights, a=math.sqrt(5))
         fan_in, _ = nn.init._calculate_fan_in_and_fan_out(self.weights)
         bound = 1 / math.sqrt(fan_in)
         nn.init.uniform_(self.bias, -bound, bound)
 
     def forward(self, x):
-
-        x = rearrange(x, 'b (c m) h w -> b m c h w', m=self.m)
+        x = rearrange(x, "b (c m) h w -> b m c h w", m=self.m)
         x = torch.zeros_like(x)
 
         for i in range(self.m):
             x[:, i, :, :, :] = nn.functional.conv2d(
-                x[:, i, :, :, :], self.weights[i], bias=None, stride=1, padding=self.kernel_size//2, dilation=1, groups=1)
+                x[:, i, :, :, :],
+                self.weights[i],
+                bias=None,
+                stride=1,
+                padding=self.kernel_size // 2,
+                dilation=1,
+                groups=1,
+            )
 
-        x = rearrange(x, 'b m c h w -> b (c m) h w', m=self.m)
+        x = rearrange(x, "b m c h w -> b (c m) h w", m=self.m)
 
         return x + self.bias.view(1, -1, 1, 1)
+
 
 def head_conv2d(x, weights, bias, kernel_size):
     """
@@ -113,42 +123,43 @@ def head_conv2d(x, weights, bias, kernel_size):
     """
 
     result = nn.functional.conv2d(
-            x, weights, bias=bias, stride=1, padding=kernel_size//2, dilation=1, groups=1)
-    
+        x, weights, bias=bias, stride=1, padding=kernel_size // 2, dilation=1, groups=1
+    )
+
     return result
+
 
 head_conv2d_vmap = torch.vmap(head_conv2d, in_dims=(0, 0, 0, None), out_dims=0)
 
+
 class BlockDiagonalConv2d_v2(nn.Module):
-
     def __init__(self, in_channels, out_channels, kernel_size, m):
-
         super(BlockDiagonalConv2d_v2, self).__init__()
         self.in_channels = in_channels
         self.out_channels = out_channels
         self.kernel_size = kernel_size
         self.m = m
-        self.weights = nn.Parameter(torch.randn(
+        self.weights = nn.Parameter(
+            torch.randn(
+                m, out_channels // m, in_channels // m, kernel_size, kernel_size
+            )
+        )
 
-            m, out_channels//m, in_channels//m, kernel_size, kernel_size))
-
-        self.bias = nn.Parameter(torch.zeros(m, out_channels//m))
+        self.bias = nn.Parameter(torch.zeros(m, out_channels // m))
 
         self.reset_parameters()
 
     def reset_parameters(self):
-        
         nn.init.kaiming_uniform_(self.weights, a=math.sqrt(5))
         fan_in, _ = nn.init._calculate_fan_in_and_fan_out(self.weights)
         bound = 1 / math.sqrt(fan_in)
         nn.init.uniform_(self.bias, -bound, bound)
 
     def forward(self, x):
-
-        x = rearrange(x, 'b (c m) h w -> m b c h w', m=self.m)
+        x = rearrange(x, "b (c m) h w -> m b c h w", m=self.m)
 
         res = head_conv2d_vmap(x, self.weights, self.bias, self.kernel_size)
 
-        res = rearrange(res, 'm b c h w -> b (c m) h w', m=self.m)
+        res = rearrange(res, "m b c h w -> b (c m) h w", m=self.m)
 
         return res
